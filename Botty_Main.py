935d9b6a426515c5689772f1c035d643f658f30d
Johnny Lin <tsunglin.lin@hotmail.com>
2018-10-19 16:22:12 +0800

    123

--- Botty_Main.py
+++ Botty_Main.py
@@ -61,11 +61,11 @@ def handle_message(event):
     #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
     #print("message.id_type: " + str(event.message.id))
     id = event.message.id
-    print(event.source)
+    print(event.source.type)
     #print(type(id))
 
     message_content = line_bot_api.get_message_content(id)
-
+    ."userId"
 
     #Save Audio File#######################################
 
