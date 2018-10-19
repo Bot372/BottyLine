2e5d5bf3221e89b8129b47906c919be008ab1842
Johnny Lin <tsunglin.lin@hotmail.com>
2018-10-19 17:54:52 +0800

    123

--- Botty_Main.py
+++ Botty_Main.py
@@ -61,7 +61,7 @@ def handle_message(event):
     print( event )
     event_S =  str(event)
     event_S = event_S[event_S.find("userId") + 10: len(event_S)]
-    event_S = event_S[0: event_S.find("type") - 4]
+    event_S = event_S[0: event_S.find("timestamp") - 4]
 
 
 
