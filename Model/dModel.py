57826370df0733e27e66baad8dbe2a85a0574b99
Johnny Lin <tsunglin.lin@hotmail.com>
2018-10-26 12:59:19 +0800

    db

--- Model/dModel.py
+++ Model/dModel.py
@@ -4,6 +4,6 @@ from Entity.Entity import *
 class UserData(db.Model):
     __tablename__ = 'UserData'
 
-    def __init__(self, id ,line_id):
-        self.id = id
+    def __init__(self, line_id):
+        #self.id = id
         self.line_id = line_id
--- data_action.py
+++ data_action.py
@@ -3,9 +3,9 @@ import uuid
 
 
 def newData(user_id):
-    ids = str(uuid.uuid4())
+    #ids = str(uuid.uuid4())
     line_id = str(user_id)
-    insert_data = UserData(id=ids, line_id=line_id)
+    insert_data = UserData(line_id=line_id)
     db.session.add( insert_data )
     db.session.commit()
     print("DONE")
