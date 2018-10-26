82214937ddf6588228817a85ec5d725d15ca45bc
Johnny Lin <tsunglin.lin@hotmail.com>
2018-10-26 11:03:40 +0800

    db

--- data_action.py
+++ data_action.py
@@ -4,7 +4,7 @@ import uuid
 
 def newData(user_id):
     ids = str(uuid.uuid4())
-    line_id = user_id
+    line_id = str(user_id)
     insert_data = UserData(id=ids, line_id=line_id)
     db.session.add( insert_data )
     db.session.commit()
